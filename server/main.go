8e4477a3f33304c912f5824d074f6d636b1140e9
xavier-kong <kongxianchengxavier@gmail.com>
2022-09-16 21:54:11 +0800

    sounds like this could be a little harder than i thought i would be

--- server/main.go
+++ server/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os/exec"
+)
 
+func main() {
+	cmd := exec.Command("./test.py")
+	out, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(out))
 }
