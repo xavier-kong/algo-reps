7b683c3fd009390cd8f0d111cc5c3489f5fcd74d
xavier-kong <kongxianchengxavier@gmail.com>
2023-01-05 20:31:30 +0800

    light mode hurts my soul

--- client/components/grid.tsx
+++ client/components/grid.tsx
@@ -32,16 +32,16 @@ function createTopicHeader(topic: string): string {
 
 function createCard(topic: string) {
   return (
-    <div className="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
+    //<div className="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">
       <Card imgSrc={`/assets/topics/${topic}.jpeg`}>
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 className="text-lg md:text-base font-bold tracking-tight text-gray-900 dark:text-white">
           {createTopicHeader(topic)}
         </h5>
         {/*<p className="font-normal text-gray-700 dark:text-gray-400">
           Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
         </p>*/}
       </Card>
-    </div>
+    //</div>
   )
 }
 
